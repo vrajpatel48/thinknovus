9d8c7ca9c65d6d5b98ff87b06df5563f3d0cd130
vrajpatel <121706070+vrajpatel48@users.noreply.github.com>
2024-06-05 16:59:49 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        "medi-green": "#96BB7C",
+      },
+    },
+  },
+  plugins: [],
+};
